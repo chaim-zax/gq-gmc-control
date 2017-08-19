292e2c218f7846aea3c394a952c4f48d1fd770df
Chaim Zax <chaim.zax@imec-nl.nl>
2017-08-19 21:12:22 +0200

    Support for conversion to Sievert.

--- gq-gmc-control.py
+++ gq-gmc-control.py
@@ -16,6 +16,9 @@ if platform.system() == 'Windows':
 else:
     DEFAULT_PORT = '/dev/ttyUSB0'
 DEFAULT_BAUDRATE = 115200
+DEFAULT_CPM_TO_SIEVERT = '1000,6.50'
+DEFAULT_OUTPUT_IN_CPM = False
+DEFAULT_NO_PARSE = False
 
 EOL = '\n'
 VERSION = '1.0.0'
@@ -23,6 +26,7 @@ VERSION = '1.0.0'
 def handleArguments():
     parser = argparse.ArgumentParser(description='Control tool for the GQ GMC-500 series.',
                                      epilog="Copyright (c) 2017, Chaim Zax")
+    unit_group = parser.add_mutually_exclusive_group()
     group = parser.add_argument_group(title='commands', description='Device specific commands. Any of the following commands below can be send to de device. Only one command can be used at a time.')
     command_group = group.add_mutually_exclusive_group(required=True)
 
@@ -36,6 +40,12 @@ def handleArguments():
                        help='do not parse the history data into a csv file, only store the binary data. use only in combination with the \'--data\' command option.')
     parser.add_argument('-c', '--config', action='store', default=None,
                        help='load command line options from a configuration file')
+    #unit_group.add_argument('-S', '--output-in-sievert', metavar='CPM,Sievert', nargs='?',
+    #                    help='don\'t log data in CPM or CPS but in micro Sieverts. optionally supply a tuple used as a conversion factor. e.g. \'1000,0.0000065\' indicates 1000 CPM equals 6.50 uSievert.')
+    unit_group.add_argument('-S', '--output-in-usievert', metavar='CPM,uSievert', nargs='?', dest='output_in_usievert', const='',
+                            help='don\'t log data in CPM or CPS but in micro Sieverts. optionally supply a tuple used as a conversion factor. e.g. \'1000,6.50\' indicates 1000 CPM equals 6.50 uSievert.')
+    unit_group.add_argument('-M', '--output-in-cpm', action='store_true', default=None,
+                            help='log data in CPM or CPS')
     parser.add_argument('output_file', metavar='OUTPUT_FILE', nargs='?',
                         help='an output file')
 
@@ -62,16 +72,16 @@ def handleArguments():
                        help='get the current gyroscopic data')
     command_group.add_argument('-d', '--data', action='store_true', default=None,
                        help='download all history data and store it to file (default \'%s\' or \'%s\'). can be used in combination with the \'--no-parse\' option' % (DEFAULT_CSV_FILE, DEFAULT_BIN_FILE))
-    command_group.add_argument('-P', '--only-parse', nargs="?", type=str, dest="bin_file", const="",
+    command_group.add_argument('-P', '--only-parse', nargs='?', type=str, dest='bin_file', const='',
                        help='do not download history data, only parse the already downloaded data to a csv file (default \'%s\'). can be used in combination with the \'--data\' option to create a csv file with a different file-name' % (DEFAULT_CSV_FILE))
     command_group.add_argument('-l', '--list-config', action='store_true', default=None,
-                       help='shows the current configuration')
+                       help='shows the current device configuration')
     command_group.add_argument('-e', '--erase-config', action='store_true', default=None,
-                       help='erase the current configuration')
+                       help='erase the current device configuration')
     command_group.add_argument('-w', '--write-config', action='store_true', default=None,
-                       help='write the configuration')
+                       help='write the device configuration')
     command_group.add_argument('-u', '--update-config', action='store_true', default=None,
-                       help='update the current configuration')
+                       help='update the current device configuration')
     command_group.add_argument('-t', '--set-time', action='store', default=None,
                        help='set the local time')
     command_group.add_argument('-D', '--set-date', action='store', default=None,
@@ -85,7 +95,7 @@ def handleArguments():
     command_group.add_argument('-r', '--reboot', action='store_true', default=None,
                        help='reboot the device')
 
-    command_group.add_argument('-S', '--show-config', action='store_true', default=None,
+    command_group.add_argument('-L', '--list-tool-config', action='store_true', default=None,
                        help='shows the currently used command line configuration options')
     command_group.add_argument('-v', '--version', action='version', version='%(prog)s ' + VERSION)
 
@@ -226,36 +236,46 @@ def getData(address=0x000000, length=None, out_file=DEFAULT_BIN_FILE):
 
     f_out.close
 
-def printData(out_file, data_type, c_str, size=1):
-    c0 = ord(c_str[0])
+def cpmToUSievert(cpm, unit, cpm_to_usievert):
+    if cpm_to_usievert == None:
+        return (cpm, unit)
 
-    if size == 1:
-        if c0 != 0xff:
-            return('%d,%s' % (c0, data_type))
+    if unit == 'CPS':
+        return (cpm * cpm_to_usievert[1] / cpm_to_usievert[0] * 60, 'uSv/h')
+    elif unit == 'CPM':
+        return (cpm * cpm_to_usievert[1] / cpm_to_usievert[0], 'uSv/h')
+    elif unit == 'CPH':
+        return (cpm * cpm_to_usievert[1] / cpm_to_usievert[0] / 60, 'uSv/h')
+    else:
+        return (cpm, unit)
 
-    elif size == 2:
-        c1 = ord(c_str[1])
-        if c0 != 0xff:
-            return('%d,%s' % (c0 * 256 + c1, data_type))
 
-    elif size == 3:
-        c1 = ord(c_str[1])
-        c2 = ord(c_str[2])
-        if c0 != 0xff:
-            return('%d,%s' % (c0 * 256 * 256 + c1 * 256 + c2, data_type))
-        else:
-            return(None)
+def printData(out_file, data_type, c_str, size=1, cpm_to_usievert=None):
+    value = (None, None)
+
+    if size < 5:
+        c_value = 0
+        for i in range(size):
+            c_value = c_value * 256 + ord(c_str[i])
+        value = cpmToUSievert(c_value, data_type, cpm_to_usievert)
 
     else:
         return('(unsupported size: %d)' % (size))
 
+    if value[1] == None or value[1] == '':
+        return(None)
+    elif value[1] == 'uSv/h':
+        return('%.4f,%s' % value)
+    else:
+        return('%d,%s' % value)
 
-def parseDataFile(in_file=DEFAULT_BIN_FILE, out_file=DEFAULT_CSV_FILE):
+def parseDataFile(in_file=DEFAULT_BIN_FILE, out_file=DEFAULT_CSV_FILE, cpm_to_usievert=None):
     if in_file == None:
         in_file = DEFAULT_BIN_FILE
     print("parsing file '" + in_file + "', and storing data to '" + out_file + "'")
 
     marker = 0
+    #eof_count = 0
     data_type = '*'
     f_in = open(in_file, 'r')
     f_out = open(out_file, 'w')
@@ -292,16 +312,18 @@ def parseDataFile(in_file=DEFAULT_BIN_FILE, out_file=DEFAULT_CSV_FILE):
                 f_out.write(',,20%02d/%02d/%02d %02d:%02d:%02d,%s' % (ord(date[0]), ord(date[1]), ord(date[2]), ord(date[3]), ord(date[4]), ord(date[5]), mode_str) + EOL)
             elif c == 0x01:
                 data = f_in.read(2)
-                value = printData(f_out, data_type, data, size=2)
+                value = printData(f_out, data_type, data, size=2, cpm_to_usievert=cpm_to_usievert)
                 f_out.write(value + EOL)
 
             elif c == 0x02:
                 data = f_in.read(3)
-                value = printData(f_out, data_type, data, size=3)
+                value = printData(f_out, data_type, data, size=3, cpm_to_usievert=cpm_to_usievert)
                 f_out.write(value + EOL)
 
-            elif c == 0x03:
-                f_out.write(',,,,[3?]' + cmd + EOL)
+            elif c == 0x03:  # TODO: test me ;)
+                data = f_in.read(4)
+                value = printData(f_out, data_type, data, size=4, cpm_to_usievert=cpm_to_usievert)
+                f_out.write(value + EOL)
 
             elif c == 0x04:  # note
                 length = ord(f_in.read(1))
@@ -323,9 +345,17 @@ def parseDataFile(in_file=DEFAULT_BIN_FILE, out_file=DEFAULT_CSV_FILE):
             marker = 0x55
             continue
 
-        value = printData(f_out, data_type, c_str, size=1)
+        value = printData(f_out, data_type, c_str, size=1, cpm_to_usievert=cpm_to_usievert)
         if value != None:
             f_out.write(value + EOL)
+
+        # detect end of file
+        #if ord(c_str[0]) == 0xff:
+        #    eof_count += 1
+        #    if eof_count == 100:
+        #        break
+        #else:
+        #    eof_count = 0
 
     f_in.close()
     f_out.close()
@@ -358,7 +388,9 @@ if __name__ == "__main__":
     port = DEFAULT_PORT
     bin_file = DEFAULT_BIN_FILE
     output_file = DEFAULT_CSV_FILE
-    no_parse = False
+    no_parse = DEFAULT_NO_PARSE
+    output_in_usievert = DEFAULT_CPM_TO_SIEVERT
+    output_in_cpm = DEFAULT_OUTPUT_IN_CPM
 
     home = os.path.expanduser("~")
     default_config = DEFAULT_CONFIG.replace('~', home)
@@ -378,14 +410,29 @@ if __name__ == "__main__":
         no_parse = args.no_parse
     if args.output_file != None:
         output_file = args.output_file
+    if args.output_in_usievert != None and args.output_in_usievert != '':
+        output_in_usievert = args.output_in_usievert
+    if args.output_in_cpm != None:
+        output_in_cpm = args.output_in_cpm
 
-    if args.show_config == True:
-        print("baudrate    = %s" % (baudrate))
-        print("port        = '%s'" % (port))
-        print("bin_file    = '%s'" % (bin_file))
-        print("output_file = '%s'" % (output_file))
-        print("no_parse    = %s" % (no_parse))
+    if args.list_tool_config == True:
+        print("baudrate           = %s" % (baudrate))
+        print("port               = '%s'" % (port))
+        print("bin_file           = '%s'" % (bin_file))
+        print("output_file        = '%s'" % (output_file))
+        print("no_parse           = %s" % (no_parse))
+        print("output_in_usievert = '%s'" % (output_in_usievert))
+        print("output_in_cpm      = %s" % (output_in_cpm))
         sys.exit(0)
+
+
+    cpm_to_usievert = None
+    if output_in_cpm == False or args.output_in_usievert != None:
+        conversion = output_in_usievert.split(',')
+        if len(conversion) != 2:
+            print("WARNING: conversion to Sieverts not valid, defaulting to CPM/CPS")
+        else:
+            cpm_to_usievert = (int(conversion[0]), float(conversion[1]))
 
     # handle all operations
     if no_parse == True and args.data != True:
@@ -393,7 +440,7 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     if args.bin_file != None:
-        parseDataFile(bin_file, output_file)
+        parseDataFile(bin_file, output_file, cpm_to_usievert=cpm_to_usievert)
         sys.exit(0)
 
     res = openDevice(port=port, baudrate=baudrate)
@@ -415,7 +462,7 @@ if __name__ == "__main__":
         getData(out_file=bin_output_file)
 
         if no_parse == False:
-            parseDataFile(bin_output_file, output_file)
+            parseDataFile(bin_output_file, output_file, cpm_to_usievert=cpm_to_usievert)
 
         if tmp_file != None and os.path.exists(tmp_file):
             os.remove(tmp_file)
