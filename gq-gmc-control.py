c37c1bc6f15e42387beeae30a95277e6030ce094
Chaim Zax <chaim.zax@imec-nl.nl>
2017-08-19 21:20:26 +0200

    Adding copyright notice.

--- gq-gmc-control.py
+++ gq-gmc-control.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  Copyright (c) 2017, Chaim Zax <chaim.zax@gmail.com>
 
 import sys
 import os
@@ -25,7 +41,7 @@ VERSION = '1.0.0'
 
 def handleArguments():
     parser = argparse.ArgumentParser(description='Control tool for the GQ GMC-500 series.',
-                                     epilog="Copyright (c) 2017, Chaim Zax")
+                                     epilog="Copyright (c) 2017, Chaim Zax <chaim.zax@gmail.com>")
     unit_group = parser.add_mutually_exclusive_group()
     group = parser.add_argument_group(title='commands', description='Device specific commands. Any of the following commands below can be send to de device. Only one command can be used at a time.')
     command_group = group.add_mutually_exclusive_group(required=True)
