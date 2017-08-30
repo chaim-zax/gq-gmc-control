39d436e5c46fd857ef4a9fe13afef0156d5ca25d
Chaim Zax <chaim.zax@gmail.nl>
2017-08-30 22:14:46 +0200

    Support for unit conversion from device calibration values, and setting verbosity level.

--- gq-gmc-control.py
+++ gq-gmc-control.py
@@ -38,9 +38,11 @@ DEFAULT_CPM_TO_SIEVERT = '1000,6.50'
 DEFAULT_OUTPUT_IN_CPM = False
 DEFAULT_NO_PARSE = False
 DEFAULT_SKIP_CHECK = False
+DEFAULT_UNIT_CONVERSION_FROM_DEVICE = False
 DEFAULT_DEVICE_TYPE = None
 DEFAULT_FLASH_SIZE = 0x00100000  # 1 MByte
 DEFAULT_CONFIGUATION_SIZE = 0x100 # 256 byte
+DEFAULT_VERBOSE_LEVEL = 2
 
 EOL = '\n'
 VERSION = '1.0.0'
@@ -75,6 +77,8 @@ m_deviceType = None
 m_deviceName = DEFAULT_DEVICE_TYPE
 m_port = None
 m_configData = None
+m_config = None
+m_verbose = DEFAULT_VERBOSE_LEVEL
 
 def handleArguments():
     parser = argparse.ArgumentParser(description='Control tool for the GQ GMC Geiger Counters. This tool provices a convenient command line user interface to most of the device features (which are accessable by usb). Currently the GMC-280, GMC-300, GMC-320 and GMC-500 models are supported.',
@@ -105,6 +109,10 @@ def handleArguments():
                        help="don't use the autodetect feature to select the device-type, but use the one provided ('CMG-280', 'CMG-300', 'CMG-320' or 'CMG-500')")
     parser.add_argument('output_file', metavar='OUTPUT_FILE', nargs='?',
                         help='an output file')
+    parser.add_argument('-u', '--unit-conversion-from-device', action='store_true', default=None,
+                        help="use the CPM to Sievert calibration values from the device to convert data to uSieverts.")
+    parser.add_argument('-B', '--verbose', action='store', default=None, type=int, choices=[1,2],
+                        help="in- or decrease verbosity")
 
     # device operations
     command_group.add_argument('-i', '--device-info', action='store_true', default=None,
@@ -143,7 +151,7 @@ def handleArguments():
                        choices=['S1', 'S2', 'S3', 'S4'],
                        help='emulate a keypress of the device')
     command_group.add_argument('-F', '--firmware-update', action='store', default=None,
-                       help='update the firmware of the device')
+                       help='update the firmware of the device (NOT IMPLEMENTED)')
     command_group.add_argument('-R', '--reset', action='store_true', default=None,
                        help='reset the device to the default factory settings')
     command_group.add_argument('-r', '--reboot', action='store_true', default=None,
@@ -161,6 +169,16 @@ def validDateTime(s):
     except ValueError:
         msg = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
+
+def commandReturnedOK():
+    for loop in range(10):
+        ret = m_port.read(1)
+        if ret != '':
+            break
+
+    if ret == '' or ord(ret) != 0xaa:
+        return False
+    return True
 
 def clearPort():
     # close any pending previous command
@@ -182,7 +200,8 @@ def checkDeviceType():
        m_deviceName == 'GMC-300' or \
        m_deviceName == 'GMC-320' or \
        m_deviceName == 'GMC-500':
-        print("device found: %s" % m_deviceType)
+        if m_verbose == 2:
+            print("device found: %s" % m_deviceType)
 
     elif m_deviceName[:3] == 'GMC':
         print("WARNING: device found (%s) but officially not supported, using defaults" % m_deviceType)
@@ -222,8 +241,12 @@ def setPower(on=True):
 
     if on:
         m_port.write('<POWERON>>')
+        if m_verbose == 2:
+            print('device power on')
     else:
         m_port.write('<POWEROFF>>')
+        if m_verbose == 2:
+            print('device power off')
 
 def getVoltage():
     if m_port == None:
@@ -232,9 +255,9 @@ def getVoltage():
 
     m_port.write('<GETVOLT>>')
     voltage = m_port.read(3)
-    return voltage
+    return('%s V' % voltage)
 
-def getCPM():
+def getCPM(cpm_to_usievert=None):
     if m_port == None:
         print('ERROR: no device connected')
         return -1
@@ -242,7 +265,15 @@ def getCPM():
     m_port.write('<GETCPM>>')
     cpm = m_port.read(2)
     value = struct.unpack(">H", cpm)[0]
-    return value
+
+    unit_value = (value, 'CPM')
+    if cpm_to_usievert != None:
+        unit_value = cpmToUSievert(value, 'CPM', cpm_to_usievert)
+
+    if unit_value[1] == 'uSv/h':
+        return('%.4f %s' % unit_value)
+    else:
+        return('%d %s' % unit_value)
 
 def getData(address=0x000000, length=None, out_file=DEFAULT_BIN_FILE):
     if m_port == None:
@@ -263,7 +294,8 @@ def getData(address=0x000000, length=None, out_file=DEFAULT_BIN_FILE):
     sub_addr = address
     sub_len = 4096
 
-    print("storing data to '" + out_file + "'")
+    if m_verbose == 1:
+        print("storing data to '" + out_file + "'")
     f_out = open(out_file, 'w')
 
     while True:
@@ -276,7 +308,8 @@ def getData(address=0x000000, length=None, out_file=DEFAULT_BIN_FILE):
 
         f_out.write(data)
         total_len += len(data)
-        print("address: 0x%06x, size: %s, total size: %d bytes (%d%%)" % (sub_addr, sub_len, total_len, int(total_len*100/length)))
+        if m_verbose == 2:
+            print("address: 0x%06x, size: %s, total size: %d bytes (%d%%)" % (sub_addr, sub_len, total_len, int(total_len*100/length)))
         sub_addr += sub_len
 
     f_out.close
@@ -294,6 +327,23 @@ def cpmToUSievert(cpm, unit, cpm_to_usievert):
     else:
         return (cpm, unit)
 
+def getUnitConversionFromDevice():
+    # make sure the cached config is up to date
+    if m_configData == None:
+        getConfig()
+
+    cal1_sv = m_config['cal1_sv'] * 1000 / m_config['cal1_cpm']
+    cal2_sv = m_config['cal2_sv'] * 1000 / m_config['cal2_cpm']
+    cal3_sv = m_config['cal3_sv'] * 1000 / m_config['cal3_cpm']
+    cal_sv = (cal1_sv + cal2_sv + cal3_sv) / 3
+
+    if m_verbose == 2:
+        print("calibration value 1 from device: %d cpm = %.2f uSievert" % (m_config['cal1_cpm'], m_config['cal1_sv']))
+        print("calibration value 2 from device: %d cpm = %.2f uSievert" % (m_config['cal2_cpm'], m_config['cal2_sv']))
+        print("calibration value 3 from device: %d cpm = %.2f uSievert" % (m_config['cal3_cpm'], m_config['cal3_sv']))
+        print("using the average of calibration values: %d cpm = %.2f uSievert" % (1000, cal_sv))
+
+    return (1000, cal_sv)
 
 def printData(out_file, data_type, c_str, size=1, cpm_to_usievert=None):
     value = (None, None)
@@ -317,7 +367,8 @@ def printData(out_file, data_type, c_str, size=1, cpm_to_usievert=None):
 def parseDataFile(in_file=DEFAULT_BIN_FILE, out_file=DEFAULT_CSV_FILE, cpm_to_usievert=None):
     if in_file == None:
         in_file = DEFAULT_BIN_FILE
-    print("parsing file '" + in_file + "', and storing data to '" + out_file + "'")
+    if m_verbose == 1:
+        print("parsing file '" + in_file + "', and storing data to '" + out_file + "'")
 
     marker = 0
     #eof_count = 0
@@ -435,7 +486,8 @@ def setHeartbeat(enable):
             sys.stdout.write('.')
             if x == '':
                 break
-        print("ok")
+        if m_verbose == 2:
+            print("ok")
 
 def getTemperature():
     if m_port == None:
@@ -447,7 +499,7 @@ def getTemperature():
     sign = ''
     if ord(temp[2]) != 0:
         sign = '-'
-    return "%s%d.%d" % (sign, ord(temp[0]), ord(temp[1]))
+    return("%s%d.%d %s%s" % (sign, ord(temp[0]), ord(temp[1]), unichr(0x00B0), unichr(0x0043)))
 
 def getGyro():
     if m_port == None:
@@ -460,7 +512,7 @@ def getGyro():
     return "x:%d, y:%d, z:%d" % (x, y, z)
 
 def getConfig():
-    global m_configData
+    global m_configData, m_config
 
     if m_port == None:
         print('ERROR: no device connected')
@@ -473,7 +525,31 @@ def getConfig():
 
     m_port.write('<GETCFG>>')
     data = m_port.read(size)
+
+    if data == '' or len(data) == 0:
+        print("WARNING: reading device configuration failed")
+        return -1
+
     m_configData = ctypes.create_string_buffer(data)
+
+    m_config = {}
+    m_config['cal1_cpm'] = ord(m_configData[ADDRESS_CALIBRATE1_CPM]) * 256 + ord(m_configData[ADDRESS_CALIBRATE1_CPM+1])
+    m_config['cal1_sv'] = struct.unpack(">f", m_configData[ADDRESS_CALIBRATE1_SV:ADDRESS_CALIBRATE1_SV+4])[0]
+    m_config['cal2_cpm'] = ord(m_configData[ADDRESS_CALIBRATE2_CPM]) * 256 + ord(m_configData[ADDRESS_CALIBRATE2_CPM+1])
+    m_config['cal2_sv'] = struct.unpack(">f", m_configData[ADDRESS_CALIBRATE2_SV:ADDRESS_CALIBRATE2_SV+4])[0]
+    m_config['cal3_cpm'] = ord(m_configData[ADDRESS_CALIBRATE3_CPM]) * 256 + ord(m_configData[ADDRESS_CALIBRATE3_CPM+1])
+    m_config['cal3_sv'] = struct.unpack(">f", m_configData[ADDRESS_CALIBRATE3_SV:ADDRESS_CALIBRATE3_SV+4])[0]
+    m_config['server_website'] = m_configData[ADDRESS_SERVER_WEBSITE:ADDRESS_SERVER_WEBSITE+32]
+    m_config['server_url'] = m_configData[ADDRESS_SERVER_URL:ADDRESS_SERVER_URL+32]
+    m_config['user_id'] = m_configData[ADDRESS_USER_ID:ADDRESS_USER_ID+16]
+    m_config['counter_id'] = m_configData[ADDRESS_COUNTER_ID:ADDRESS_COUNTER_ID+16]
+    if ord(m_configData[ADDRESS_WIFI_ON_OFF]) == 255:
+        m_config['wifi_active'] = True
+    else:
+        m_config['wifi_active'] = False
+    m_config['wifi_ssid'] = m_configData[ADDRESS_WIFI_SSID:ADDRESS_WIFI_SSID+16]
+    m_config['wifi_password'] = m_configData[ADDRESS_WIFI_PASSWORD:ADDRESS_WIFI_PASSWORD+16]
+    # TODO: figure out the other configuration parameters...
 
 def listConfig():
     # make sure the cached config is up to date
@@ -482,25 +558,16 @@ def listConfig():
 
     dumpData(m_configData)
 
-    print("server website: %s" % m_configData[ADDRESS_SERVER_WEBSITE:ADDRESS_SERVER_WEBSITE+32])
-    print("server url: %s" % m_configData[ADDRESS_SERVER_URL:ADDRESS_SERVER_URL+32])
-    print("user id: %s" % m_configData[ADDRESS_USER_ID:ADDRESS_USER_ID+16])
-    print("counter id: %s" % m_configData[ADDRESS_COUNTER_ID:ADDRESS_COUNTER_ID+16])
-    if ord(m_configData[ADDRESS_WIFI_ON_OFF]) == 255:
-        print("wifi: on")
-    else:
-        print("wifi: off")
-    print("wifi ssid: %s" % m_configData[ADDRESS_WIFI_SSID:ADDRESS_WIFI_SSID+16])
-    print("wifi password: %s" % m_configData[ADDRESS_WIFI_PASSWORD:ADDRESS_WIFI_PASSWORD+16])
-    cal1_cpm = ord(m_configData[ADDRESS_CALIBRATE1_CPM]) * 256 + ord(m_configData[ADDRESS_CALIBRATE1_CPM+1])
-    cal1_sv = struct.unpack(">f", m_configData[ADDRESS_CALIBRATE1_SV:ADDRESS_CALIBRATE1_SV+4])[0]
-    print("calibrate 1: %d cpm = %.2f sv" % (cal1_cpm, cal1_sv))
-    cal2_cpm = ord(m_configData[ADDRESS_CALIBRATE2_CPM]) * 256 + ord(m_configData[ADDRESS_CALIBRATE2_CPM+1])
-    cal2_sv = struct.unpack(">f", m_configData[ADDRESS_CALIBRATE2_SV:ADDRESS_CALIBRATE2_SV+4])[0]
-    print("calibrate 2: %d cpm = %.2f sv" % (cal2_cpm, cal2_sv))
-    cal3_cpm = ord(m_configData[ADDRESS_CALIBRATE3_CPM]) * 256 + ord(m_configData[ADDRESS_CALIBRATE3_CPM+1])
-    cal3_sv = struct.unpack(">f", m_configData[ADDRESS_CALIBRATE3_SV:ADDRESS_CALIBRATE3_SV+4])[0]
-    print("calibrate 3: %d cpm = %.2f sv" % (cal3_cpm, cal3_sv))
+    print("server website: %s" % m_config['server_website'])
+    print("server url: %s" % m_config['server_url'])
+    print("user id: %s" % m_config['user_id'])
+    print("counter id: %s" % m_config['counter_id'])
+    print("wifi active: " + str(m_config['wifi_active']))
+    print("wifi ssid: %s" % m_config['wifi_ssid'])
+    print("wifi password: %s" % m_config['wifi_password'])
+    print("calibrate 1: %d cpm = %.2f sv" % (m_config['cal1_cpm'], m_config['cal1_sv']))
+    print("calibrate 2: %d cpm = %.2f sv" % (m_config['cal2_cpm'], m_config['cal2_sv']))
+    print("calibrate 3: %d cpm = %.2f sv" % (m_config['cal3_cpm'], m_config['cal3_sv']))
 
 def writeConfig(parameters):
     global m_configData
@@ -508,9 +575,9 @@ def writeConfig(parameters):
     if m_deviceName == 'GMC-280' or \
        m_deviceName == 'GMC-300' or \
        m_deviceName == 'GMC-320':
-        address_size = 'B'
+        address_size = 'B'  # 1 byte address
     elif m_deviceName == 'GMC-500':
-        address_size = 'H'
+        address_size = 'H'  # 2 byte address
     else:
         print('ERROR: device not supported, feature not available')
         return
@@ -519,6 +586,7 @@ def writeConfig(parameters):
     if m_configData == None:
         getConfig()
 
+    # update the cached configuration (in memory)
     for par in parameters:
         par_value = par.split('=')
         if len(par_value) != 2:
@@ -541,9 +609,9 @@ def writeConfig(parameters):
         else:
             print("WARNING: parameter with name '%s' not supported" % par_value[0])
 
+    # erase all stored parameters in flash
     m_port.write('<ECFG>>')
-    ret = m_port.read(1)
-    if ord(ret) != 0xaa:
+    if not commandReturnedOK():
         print("WARNING: erase operation failed, parameters not stored on device")
         return
 
@@ -552,24 +620,18 @@ def writeConfig(parameters):
     else:
         size = DEFAULT_CONFIGUATION_SIZE
 
+    # write all cached parameters (from memory) into the device
     for i in range(size):
         cmd = struct.pack('>' + address_size + 'B', i, ord(m_configData[i]))
         m_port.write('<WCFG' + cmd + '>>')
-        ret = m_port.read(1)
-        if ret == '' or ord(ret) != 0xaa:
+        if not commandReturnedOK():
             print("WARNING: write operation failed at address 0x%02X, (some) parameters not stored to device" % i)
 
+    # make the change permanent (write flash)
     m_port.write('<CFGUPDATE>>')
-    ret = m_port.read(1)
-    if ord(ret) != 0xaa:
+    if not commandReturnedOK():
         print("WARNING: update operation failed, parameters not stored on device")
         return
-
-def setTime():
-    # "SETTIMEHH"...
-    # "SETTIMEMM"...
-    # "SETTIMESS"...
-    print 'option not yet available'
 
 def getDateAndTime():
     if m_port == None:
@@ -590,12 +652,7 @@ def setDateAndTime(date_time):
     cmd = struct.pack('>BBBBBB', date_time.year - 2000, date_time.month, date_time.day, date_time.hour, date_time.minute, date_time.second)
     m_port.write('<SETDATETIME' + cmd + '>>')
 
-    for loop in range(10):
-      ret = m_port.read(1)
-      if ret != '':
-          break
-
-    if ret == '' or ord(ret) != 0xaa:
+    if not commandReturnedOK():
        print("WARNING: setting date and time not succeded")
 
 def sendKey(key):
@@ -625,8 +682,7 @@ def factoryReset():
 
     m_port.write('<FACTORYRESET>>')
 
-    ret = m_port.read(1)
-    if ord(ret) != 0xaa:
+    if not commandReturnedOK():
         print("WARNING: factory reset not succeded")
 
 def reboot():
@@ -649,7 +705,7 @@ def openDevice(port=None, baudrate=115200, skip_check=False, device_type=None):
     try:
         m_port = serial.Serial(port, baudrate=baudrate, timeout=1.0)
     except serial.serialutil.SerialException:
-        print('No device found')
+        print('ERROR: No device found')
         return -1
 
     clearPort()
@@ -664,7 +720,8 @@ def openDevice(port=None, baudrate=115200, skip_check=False, device_type=None):
            device_type == 'GMC-320' or \
            device_type == 'GMC-500':
             m_deviceName = device_type
-            print("using device-type: %s" % m_deviceName)
+            if m_verbose == 2:
+                print("using device-type: %s" % m_deviceName)
         else:
             print("WARNING: unsupported selected device type '%s', defaulting to '%s'" % (device_type, m_deviceName))
 
@@ -682,7 +739,9 @@ if __name__ == "__main__":
     output_in_usievert = DEFAULT_CPM_TO_SIEVERT
     output_in_cpm = DEFAULT_OUTPUT_IN_CPM
     skip_check = DEFAULT_SKIP_CHECK
+    unit_conversion_from_device = DEFAULT_UNIT_CONVERSION_FROM_DEVICE
     device_type = DEFAULT_DEVICE_TYPE
+    verbose = DEFAULT_VERBOSE_LEVEL
 
     home = os.path.expanduser("~")
     default_config = DEFAULT_CONFIG.replace('~', home)
@@ -698,35 +757,45 @@ if __name__ == "__main__":
         port = args.port
     if args.bin_file != None and args.bin_file != '':
         bin_file = args.bin_file
-    if args.no_parse != None:
-        no_parse = args.no_parse
     if args.output_file != None:
         output_file = args.output_file
+    if args.no_parse != None:
+        no_parse = args.no_parse
     if args.output_in_usievert != None and args.output_in_usievert != '':
         output_in_usievert = args.output_in_usievert
     if args.output_in_cpm != None:
         output_in_cpm = args.output_in_cpm
     if args.skip_check != None:
         skip_check = args.skip_check
+    if args.unit_conversion_from_device != None:
+        unit_conversion_from_device = args.unit_conversion_from_device
     if args.device_type != None and len(args.device_type) > 0:
         device_type = args.device_type[0]
+    if args.verbose != None:
+        verbose = args.verbose
 
+    if args.unit_conversion_from_device != None and args.output_in_cpm != None:
+        print("ERROR: the options '--output-in-cpm' and '--unit-conversion-from-device' can not be combined")
+        sys.exit(-1)
 
     if args.list_tool_config == True:
-        print("baudrate           = %s" % (baudrate))
-        print("port               = '%s'" % (port))
-        print("bin_file           = '%s'" % (bin_file))
-        print("output_file        = '%s'" % (output_file))
-        print("no_parse           = %s" % (no_parse))
-        print("output_in_usievert = '%s'" % (output_in_usievert))
-        print("output_in_cpm      = %s" % (output_in_cpm))
-        print("skip_check         = %s" % (skip_check))
+        print("baudrate                    = %s" % (baudrate))
+        print("port                        = '%s'" % (port))
+        print("bin_file                    = '%s'" % (bin_file))
+        print("output_file                 = '%s'" % (output_file))
+        print("no_parse                    = %s" % (no_parse))
+        print("output_in_usievert          = '%s'" % (output_in_usievert))
+        print("output_in_cpm               = %s" % (output_in_cpm))
+        print("skip_check                  = %s" % (skip_check))
+        print("unit_conversion_from_device = %s" % (unit_conversion_from_device))
         if device_type == None:
-            print("device_type        = None")
+            print("device_type                 = None")
         else:
-            print("device_type        = '%s'" % (device_type))
+            print("device_type                 = '%s'" % (device_type))
+        print("verbose                     = %d" % (verbose))
         sys.exit(0)
 
+    m_verbose = verbose
 
     cpm_to_usievert = None
     if output_in_cpm == False or args.output_in_usievert != None:
@@ -745,9 +814,15 @@ if __name__ == "__main__":
         parseDataFile(bin_file, output_file, cpm_to_usievert=cpm_to_usievert)
         sys.exit(0)
 
+    if args.device_info == True:
+        skip_check = True
+
     res = openDevice(port=port, baudrate=baudrate, skip_check=skip_check, device_type=device_type)
     if res != 0:
         sys.exit(-res)
+
+    if args.unit_conversion_from_device == True:
+        cpm_to_usievert = getUnitConversionFromDevice()
 
     if args.data == True:
         tmp_file = None
@@ -791,7 +866,7 @@ if __name__ == "__main__":
         print getVoltage()
 
     elif args.cpm == True:
-        print getCPM()
+        print getCPM(cpm_to_usievert=cpm_to_usievert)
 
     elif args.temperature == True:
         print getTemperature()
